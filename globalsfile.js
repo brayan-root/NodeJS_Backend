c850d734b984520948060ab270d96eca7d5fb149
brian <mwangi.ict@gmail.com>
2024-07-16 22:41:36 +0300

    Created an arrow function

---
+++ globalsfile.js
@@ -0,0 +1,17 @@
+//console.log(global);
+
+//
+
+
+//settimeout --only runs once
+
+//global.setTimeout(() => {
+ //   console.log('This is inside the timeout');
+//}, 3000);
+
+// arrow functions
+const greet = (name) => {
+   console.log(`hello, ${name}`); //backtick before number 1 `
+};
+
+greet('Brian');
